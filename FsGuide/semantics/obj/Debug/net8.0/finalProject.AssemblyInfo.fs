13e835fb48677eb044b9efbf3a7cd991e1a2eab7
macs-02 <144596109+macs-02@users.noreply.github.com>
2026-08-25 13:24:50 +0200

    refactor: riorganizzazione progetto

--- FsGuide/semantics/obj/Debug/net8.0/finalProject.AssemblyInfo.fs
+++ FsGuide/semantics/obj/Debug/net8.0/finalProject.AssemblyInfo.fs
@@ -10,7 +10,7 @@ open System.Reflection
 [<assembly: System.Reflection.AssemblyCompanyAttribute("finalProject")>]
 [<assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")>]
 [<assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")>]
-[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25bff90d3289a6da88ea3dbddabdef73ed26d429")>]
+[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+960a730558fd2b329fda6d2cf5cc77588807ccf0")>]
 [<assembly: System.Reflection.AssemblyProductAttribute("finalProject")>]
 [<assembly: System.Reflection.AssemblyTitleAttribute("finalProject")>]
 [<assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")>]
